7f5d1a0b068eb9e078881c32fa2899a16a237e4f
georgee <jurajbosy1@gmail.com>
2023-04-10 17:51:35 +0200

    Composition API Reactive data

--- src/App.vue
+++ src/App.vue
@@ -8,27 +8,33 @@
 
 <!--! we are telling Vue that we are using TS  -->
 <script lang="ts">
-import { defineComponent } from 'vue';
+// defineComponent => is used to define the component,
+// reactive => is used to create a reactive object,
+// toRefs => is used to convert a reactive object to a plain object with reactive properties.
+import { defineComponent, reactive, toRefs } from 'vue';
 
-// we cannot use this type of writing types in our Component:
-// let age: number | string = 25;
-
-// this is way how we make the Component
 export default defineComponent({
 	name: 'App',
-	components: {},
-	// now we add a lit a bit of DATA
-	// this data func. will return object of data
-	data() {
-		return {
-			name: 'Juraj',
 
-			// so we use it like this
-			age: 25 as number | string
-		};
+	// The components property is used to declare child components of this component. In this example, there are no child components.
+	components: {},
+
+	// The setup() function is a special function in Vue.js that allows you to set up the reactive data and functions for the component. It returns an object containing reactive properties.
+	setup() {
+		// The reactive() function creates a reactive object that will be used to store the component's data.
+		// The state object contains two properties, name and age, both of which are reactive.
+		const state = reactive({
+			name: 'Juraj',
+			age: 25 as string | number
+		});
+
+		state.name = 'Jurco';
+
+		// The toRefs() function is used to convert the reactive state object to a plain object with reactive properties. This allows us to use the state properties in the template section.
+		return { ...toRefs(state) };
 	},
-	// if we want to change the calue at some point we need will use METHOD property
-	// and inside this property we will make function to actualy change the value
+
+	// The methods object contains two functions, changeName() and changeAge(), which are used to change the name and age properties of the state object.
 	methods: {
 		changeName(name: string) {
 			this.name = name;
